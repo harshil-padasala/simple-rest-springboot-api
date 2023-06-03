4be2b25eb10796b468df79ee3c8b3dc0f63da442
harshil-padasala <harshilpadsala999@gmail.com>
2023-06-03 17:16:25 +0530

    SIMPLE REST API

---
+++ src/main/java/com/employee/rest/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.employee.rest.repository;
+
+import com.employee.rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+}
---
+++ src/main/java/com/employee/rest/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.employee.rest.service;
+
+import com.employee.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
